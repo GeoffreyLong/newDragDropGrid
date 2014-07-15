a0617e5660220a4af12038a7c0f6707e40a9a8ef
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-07-15 01:16:22 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  // Takes in an element, the number of rows, and the number of columns
+  grid = (function(elm, height, width){
+    this.elm = elm;
+    this.cellHeight = 0;
+    this.cellWidth = 0;
+
+    init = (function(){
+      setCellHeight();
+      setCellWidth();
+      createCells();
+    });
+
+    setCellHeight = function(){
+      this.cellHeight = elm.height() / height;
+    };
+
+    setCellWidth = function(){
+      this.cellWidth = elm.width() / width;
+    };
+
+    createCells = function(){
+      for (var i=0; i < parseInt(height); i++){
+        for (var j=0; j < parseInt(width); j++){
+          var drop = $('<div class="drop"></div>');
+          drop.css({
+            'position':'absolute',
+            'height':this.cellHeight+'px',
+            'width':this.cellWidth+'px',
+            'top':(i)*this.cellHeight+'px',
+            'left':(j)*this.cellWidth+'px',
+            'background':'#000000'
+          });
+          drop.click(function(){
+            alert('hello');
+          });
+          drop.attr('data-isOccupied', false);
+          drop.attr('data-row', i);
+          drop.attr('data-column', j);
+          console.log(drop.attr('data-row'));
+          //drop.droppable();
+          elm.append(drop);
+        }
+      }
+    }
+
+    init();
+  })
+  grid($('#grid'), 4, 4);
+})
